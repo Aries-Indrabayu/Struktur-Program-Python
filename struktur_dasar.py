6e3e23b5d722e02b737a0a802a1270c0918b3e13
Aries-Indrabayu <indra13498@gmail.com>
2025-03-03 21:35:12 +0700

    belajar

---
+++ struktur_dasar.py
@@ -0,0 +1,10 @@
+# Konmentar di python dimulai dengan tanda pagar
+# Ini adalah contoh komentar satu baris
+
+#Pernyataan sederhana
+print('Hello Python!') # menampilkan teks di layar
+
+# Blok kode menggunakan identasi
+if True:
+    print('Kondisi benar') # blok kode didalam if
+    print('identasi adalah kunci')
